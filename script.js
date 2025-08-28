656a0a745b697a39706908b3abdf76da29595c69
VmelvinM <vmelvinm@hotmail.com>
2025-08-28 20:38:27 -0300

    java

---
+++ script.js
@@ -0,0 +1,9 @@
+var contador = document.getElementById("count");
+var Decrementar = document.getElementById("decrement")
+var Resetar= document.getElementById("reset")
+var Incrementar = document.getElementById("increment")
+
+var number = 0;
+contador.innerText = number;
+
+ 
