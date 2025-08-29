83c459388ef32a6795501a28702a7102176db286
VmelvinM <vmelvinm@hotmail.com>
2025-08-28 21:44:00 -0300

    final

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 var contador = document.getElementById("count");
-var Decrementar = document.getElementById("decrement")
-var Resetar= document.getElementById("reset")
-var Incrementar = document.getElementById("increment")
+var decrementar = document.getElementById("decrement")
+var resetar= document.getElementById("reset")
+var incrementar = document.getElementById("increment")
 
 var number = 0;
 contador.innerText = number;
+
+decrementar.addEventListener("click", function(){
+    number--;
+   color();
+    
+});
+
+incrementar.addEventListener("click", function(){
+    number++;
+   color();
+});
+
+resetar.addEventListener("click", function(){
+    number = 0;
+   color();
+});
+
+function color(){
+    contador.innerText = number;
+
+    if(number > 0){
+        contador.style.color = "#0f0";
+              return;
+    } else if(number<0){
+        contador.style.color = "#F00";
+                return;
+    }
+    contador.style.color = "#f5f5f5";
+}
 
  
